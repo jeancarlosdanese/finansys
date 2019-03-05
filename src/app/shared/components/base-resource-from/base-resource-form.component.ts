180fb03258e423c3ccdfa0ded4792c6263ce87a1
Jean Carlos Danese <jean@danese.com.br>
2019-03-05 11:08:47 -0300

    Adding BaseResourceFormComponent

---
+++ src/app/shared/components/base-resource-from/base-resource-form.component.ts
@@ -0,0 +1,136 @@
+import { OnInit, AfterContentChecked, Injector } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { BaseResourceModel } from '../../models/base-resource.model';
+import { BaseResouceService } from '../../services/base-resource.service';
+
+import { switchMap } from 'rxjs/operators';
+import toastr from 'toastr';
+
+export abstract class BaseResourceFormComponent<T extends BaseResourceModel> implements OnInit, AfterContentChecked {
+
+  currentAction: string;
+  resourceForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = undefined;
+  submittingForm = false;
+
+  protected route: ActivatedRoute;
+  protected router: Router;
+  protected formBuilder: FormBuilder;
+
+  constructor(
+    protected injector: Injector,
+    public resource: T,
+    protected resourceService: BaseResouceService<T>,
+    protected jsonDataToResoureFn: (jsonDate) => T
+  ) {
+    this.route = this.injector.get(ActivatedRoute);
+    this.router = this.injector.get(Router);
+    this.formBuilder = this.injector.get(FormBuilder);
+  }
+
+  ngOnInit() {
+    this.setCurrentAction();
+    this.buildResourceForm();
+    this.loadResource();
+  }
+
+  ngAfterContentChecked() {
+    this.setPageTitle();
+  }
+
+  submitForm() {
+    this.submittingForm = true;
+
+    if (this.currentAction === 'new') {
+      this.createResource();
+    } else {
+      this.updateResource();
+    }
+  }
+
+  // protected methods
+  protected setCurrentAction() {
+    if (this.route.snapshot.url[0].path === 'new') {
+      this.currentAction = 'new';
+    } else {
+      this.currentAction = 'edit';
+    }
+  }
+
+  protected loadResource() {
+    if (this.currentAction === 'edit') {
+      this.route.paramMap
+        .pipe(switchMap(params => this.resourceService.getById(params.get('id')))
+      )
+      .subscribe((resource) => {
+        this.resource = resource;
+        this.resourceForm.patchValue(resource);  // binds loaded resource data to resourceForm
+      },
+      error => toastr('Ocorreu um erro no servidor, tente mais tarde'));
+    }
+  }
+
+  protected setPageTitle() {
+    if (this.currentAction === 'new') {
+      this.pageTitle = this.creationPageTitle();
+    } else {
+      this.pageTitle = this.editionPageTitle();
+    }
+  }
+
+  protected creationPageTitle(): string {
+    return 'Novo';
+  }
+
+  protected editionPageTitle(): string {
+    return 'Edição';
+  }
+
+  protected createResource() {
+    const resource: T = this.jsonDataToResoureFn(this.resourceForm.value);
+    this.resourceService.create(resource)
+      .subscribe(
+        resource => this.actionsForSuccess(resource),
+        error => this.actionsForError(error)
+      );
+  }
+
+  protected updateResource() {
+    const resource: T = this.jsonDataToResoureFn(this.resourceForm.value);
+
+    this.resourceService.update(resource)
+      .subscribe(
+        resource => this.actionsForSuccess(resource),
+        error => this.actionsForError(error)
+      );
+  }
+
+  protected actionsForSuccess(resource: T) {
+    toastr.success('Solicitação processada com sucesso!');
+
+    const baseComponentPath: string = this.route.snapshot.parent.url[0].path;
+
+    // redirect/reload component page
+    this.router.navigateByUrl(baseComponentPath, { skipLocationChange: true })
+      .then(() => {
+        this.router.navigate([baseComponentPath, resource._id, 'edit']);
+      });
+  }
+
+  protected actionsForError(error: any) {
+    toastr.error('Ocorreu um erro ao processar a sua solicitação!');
+
+    this.submittingForm = false;
+
+    if (error.status === 422) {
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    } else {
+      this.serverErrorMessages = ['Falha na comunicação com o servidor. Por favor, tente mais tarde.'];
+    }
+  }
+
+  protected abstract buildResourceForm(): void;
+}
